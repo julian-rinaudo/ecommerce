b6a9bfabd658ddad733a4e99fa9de31e1640d47e
Julian <julianrinaudo18@gmail.com>
2023-03-06 17:58:09 -0300

    initial commit

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const db = require("./db");
+// const routes = require("./routes");
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api", routes);
+
+app.use("/api", (req, res) => {
+  res.sendStatus(404);
+});
+
+db.sync({ force: false }).then(() => {
+  app.listen(3001, () => {
+    console.log("Server listening at port 3001 ");
+  });
+});
