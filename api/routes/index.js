38b7d29f0d9f0be9e96f5edf0cfc1e60a91281f1
lbertelli <82457950+lbertelli@users.noreply.github.com>
2023-03-07 12:09:37 -0300

    Update index.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/',(req,res) => {
-   res.send('cambiando develop')
+   res.send('cambiando develop probando nuevo pull')
 })
 
 
