9d32508a4b6c8fbd1f890d60a1dde71d7c620267
Alexis Bermúdez <71720889+AlexBermudez-are@users.noreply.github.com>
2023-03-08 20:52:20 -0300

    Creación de la vista de grid (NoMQ)
    creación de la vista de grid sin media querys.
    Componentes creados: Body.js, Body.css / Card.js, Card.css.

---
+++ src/components/Home/Body_Home/FakeData.js
@@ -0,0 +1,8 @@
+export const FakeData = [
+    {color:"gris", tamanio:"x", disenio:1,precio:1500,stock:10,imagen:"https://cf.shopee.com.br/file/710f8022d03ad0ea11aaf3aa1f327ba6"},
+    {color:"negro", tamanio:"xs", disenio:2,precio:1500,stock:10,imagen:"https://cf.shopee.com.br/file/710f8022d03ad0ea11aaf3aa1f327ba6"},
+    {color:"azul", tamanio:"xxs", disenio:3,precio:1500,stock:10,imagen:"https://cf.shopee.com.br/file/710f8022d03ad0ea11aaf3aa1f327ba6"},
+    {color:"rojo", tamanio:"xxxs", disenio:4,precio:1500,stock:10,imagen:"https://cf.shopee.com.br/file/710f8022d03ad0ea11aaf3aa1f327ba6"},
+    {color:"amarillo", tamanio:"xxxxxs", disenio:5,precio:1500,stock:10,imagen:"https://cf.shopee.com.br/file/710f8022d03ad0ea11aaf3aa1f327ba6"},
+    {color:"blacno", tamanio:"xxxxxxs", disenio:6,precio:1500,stock:10,imagen:"https://cf.shopee.com.br/file/710f8022d03ad0ea11aaf3aa1f327ba6"}
+];
