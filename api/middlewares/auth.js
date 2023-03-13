a7f8a2ca042e60a9e7ebbc699974241f272165fd
lbertelli <leandrojbertelli@gmail.com>
2023-03-13 19:14:39 -0300

    Creacion de Token y Autenticacion. El token se guarda en Cookies

---
+++ api/middlewares/auth.js
@@ -0,0 +1,14 @@
+const { validateToken } = require("../config/tokens");
+
+function validateAuth(req, res) {
+  const token = req.cookies.token;
+  if (!token) return res.sendStatus(401);
+
+  const { user } = validateToken(token);
+  if (!user) return res.sendStatus(401);
+
+  req.user = user;
+
+}
+
+module.exports = { validateAuth };
--- api/routes/user.js
+++ api/routes/user.js
@@ -2,42 +2,42 @@ const express = require("express");
 const { User, Shirt_Model } = require("../models");
 const userRouter = express.Router();
 
+const { generateToken } = require("../config/tokens");
+
 userRouter.post("/register", (req, res) => {
   const { first_name, last_name, email, password } = req.body;
   User.create({ first_name, last_name, email, password })
     .then((user) => res.status(201).send(user))
-    .catch((err) => console.log("error desde userRouter", err));
+    .catch((err) => console.log("error al registrar el usuario", err));
 });
 
 userRouter.post("/login", (req, res) => {
   const { email, password } = req.body;
   User.findOne({ where: { email } }).then((user) => {
-    res.cookie("user", user);
-    res.send(user);
+    if (!user) return res.sendStatus(401);
+    user.validatePassword(password).then((isValid) => {
+      if (!isValid) return res.sendStatus(401);
+
+      const payload = {
+        email: user.email,
+        first_name: user.first_name,
+        last_name: user.last_name,
+      };
+
+      const token = generateToken(payload);
+
+      res.cookie("token", token);
+
+      res.send(payload);
+    });
   });
-
-  //     User.findOne({ where: { email } }).then((user) => {
-  //       if (!user) return res.sendStatus(401);
-  //       user.validatePassword(password).then((isValid) => {
-  //         if (!isValid) return res.sendStatus(401);
-
-  //         const payload = {
-  //           email: user.email,
-  //           name: user.name,
-  //           lastname: user.lastname,
-  //         };
-
-  //         const token = generateToken(payload);
-
-  //         res.cookie("token", token);
-
-  //         res.send(payload);
-  //       });
-  //     });
-  //   })
 });
 
-userRouter.post("/logout", (req, res) => {});
+userRouter.post("/logout", (req, res) => {
+  res.clearCookie("token");
+
+  res.sendStatus(204);
+});
 
 userRouter.put("/:id", (req, res) => {
   const { first_name, last_name, email } = req.body;
