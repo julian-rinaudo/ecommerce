1f2604544d6d12a751864f6a0611aabf63904f0f
Jesica Yu <jesicayu@umich.edu>
2023-03-14 13:10:39 -0300

    cart Item eliminado, ruta cart add agregada

--- api/models/index.js
+++ api/models/index.js
@@ -2,22 +2,27 @@ const User = require("./User");
 const Shirt_Model = require("./Shirt_Model");
 const Shirt_Customize = require("./Shirt_Customized");
 const Cart = require("./Cart");
-const Cart_Item = require("./Cart_Item");
 
 // User and Cart relationship
 Cart.belongsTo(User);
 User.hasOne(Cart);
 
 // Cart_Item and Cart relationship
-Cart_Item.belongsTo(Cart);
-Cart.hasMany(Cart_Item);
+Shirt_Customize.belongsTo(Cart);
+Cart.hasMany(Shirt_Customize, {
+  as: "item",
+  foreignKey: "cartId",
+});
 
 // Shirt_Customize and User relationship
 Shirt_Customize.belongsTo(User);
 User.hasMany(Shirt_Customize);
 
 // Shirt_Customize and Shirt_Model relationship
-Shirt_Customize.belongsTo(Shirt_Model, {as: 'model'});
-Shirt_Model.hasMany(Shirt_Customize, {as: 'model'});
+Shirt_Customize.belongsTo(Shirt_Model, { as: "model" });
+Shirt_Model.hasMany(Shirt_Customize, {
+  as: "customizations",
+  foreignKey: "modelId",
+}); // se agrega una clave shirtModelId a Shirt_Customize, pero al tener foreignKey la clave sera modelId
 
-module.exports = { User, Shirt_Customize, Shirt_Model, Cart, Cart_Item };
+module.exports = { User, Shirt_Customize, Shirt_Model, Cart };
