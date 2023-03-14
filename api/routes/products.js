cc20f7f1b906941f1141324f5a330dec622ebaee
garcelomarcia <marcelogarcia91@gmail.com>
2023-03-14 16:36:31 -0600

    se agrega el feature para editar los productos

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,6 +2,14 @@ const express = require("express");
 const { User, Shirt_Model, Shirt_Customize } = require("../models");
 const productsRouter = express.Router();
 
+productsRouter.get("/", (req, res) => {
+  Shirt_Model.findAll()
+    .then((products) => {
+      res.send(products);
+    })
+    .catch((error) => console.log("Error desde productRouter", error));
+});
+
 productsRouter.get("/styles", (req, res) => {
   Shirt_Model.findAll({
     where: {
@@ -16,12 +24,12 @@ productsRouter.get("/styles", (req, res) => {
 });
 
 productsRouter.get("/styles/:style/:color/:size", (req, res) => {
-  const { style,color,size } = req.params;
+  const { style, color, size } = req.params;
   Shirt_Model.findOne({
     where: {
       color: color,
       style: style,
-      size: size
+      size: size,
     },
   })
     .then((product) => {
@@ -30,18 +38,17 @@ productsRouter.get("/styles/:style/:color/:size", (req, res) => {
     .catch((error) => console.log("Error desde productRouter", error));
 });
 
-productsRouter.post("/shirtCustomized/:id",(req,res) => {
-  const {data,url} = req.body
-  const {id} = req.params;
-  User.findByPk(id)
-  .then(user => {
-     Shirt_Customize.create({urlImage : url})
-     .then(shirtCustom => {
-       shirtCustom.setUser(user.id)
-        shirtCustom.setModel(data.id)
-     })
-     .then(err => console.log('err desde productRouter',err))
-  })
-})
+productsRouter.post("/shirtCustomized/:id", (req, res) => {
+  const { data, url } = req.body;
+  const { id } = req.params;
+  User.findByPk(id).then((user) => {
+    Shirt_Customize.create({ urlImage: url })
+      .then((shirtCustom) => {
+        shirtCustom.setUser(user.id);
+        shirtCustom.setModel(data.id);
+      })
+      .then((err) => console.log("err desde productRouter", err));
+  });
+});
 
 module.exports = productsRouter;
