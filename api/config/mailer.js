c4ddbd80cb18767b7da2b6c57f1ff2d694396d05
Julian <julianrinaudo18@gmail.com>
2023-03-16 10:48:19 -0300

    send mail listo

--- api/config/mailer.js
+++ api/config/mailer.js
@@ -5,8 +5,8 @@ const transporter = nodemailer.createTransport({
     port: 587,
     secure: false,
     auth: {
-      user: "customshirtok@gmail.com",
-      pass: "pizxbzedqkwlekha",
+      user: "julianrinaudo18@gmail.com",
+      pass: "ezahydvasdweaifp",
     },
   });
 
--- src/components/Shopping/Shopping.jsx
+++ src/components/Shopping/Shopping.jsx
@@ -1,93 +1,137 @@
-import React,{useEffect, useState} from 'react'
-import {useDispatch} from "react-redux";
-import { getShoppingApi } from '../../state/ShoppingStore';
-import Card from '../Home/Body_Home/Card';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getShoppingApi } from "../../state/ShoppingStore";
+import Card from "../Home/Body_Home/Card";
+const Swal = require("sweetalert2");
 
-let arrClon = [] // arreglo clon del estado "products"
-let precioContext = 0 // controlador del precio total del carrito
-let controller = false // controlador de ejecucion del useEffect que ejecuta una peticion a la api 
+let arrClon = []; // arreglo clon del estado "products"
+let precioContext = 0; // controlador del precio total del carrito
+let controller = false; // controlador de ejecucion del useEffect que ejecuta una peticion a la api
 
 const Shopping = () => {
+  const dispatch = useDispatch();
+  const [products, setProducts] = useState([]); // Estado incial del componente shopping
+  const [contadorProductos, setcontadorProductos] = useState(0); // Cantidad de productos en el carrito
+  const [precioFinal, setprecioFinal] = useState(0); // Precio total del carrito
+  const userLoged = useSelector((state) => state.user);
+  const montoTotal = (array) => {
+    // Actualiza el monto total del carrito
+    array.map((product) => {
+      let precioSuma = (precioContext += product.precio * product.cantidad);
+      setprecioFinal(precioSuma);
+    });
+  };
 
-    const dispatch = useDispatch();
-    const [products, setProducts] = useState([]) // Estado incial del componente shopping
-    const [contadorProductos, setcontadorProductos] = useState(0) // Cantidad de productos en el carrito
-    const [precioFinal, setprecioFinal] = useState(0) // Precio total del carrito
+  useEffect(() => {
+    // getShoppingApi es una funcion del componente de redux.
+    dispatch(getShoppingApi()).then((productos) => {
+      setProducts(productos.payload); // actualiza el estado de products con la respuesta a la api
+      setcontadorProductos(productos.payload.length);
+      if (!controller) {
+        // controller evita que se duplique el monto total del carrito haciendo 2 peticiones api
+        controller = true;
+        return montoTotal(productos.payload);
+      }
+    });
+  }, []);
 
-    const montoTotal = (array) => { // Actualiza el monto total del carrito
-        array.map(product=> {
-            let precioSuma = precioContext += (product.precio * product.cantidad) 
-            setprecioFinal(precioSuma)
-        })
-    }
+  const eliminarProducto = (el, cantidad) => {
+    // Elimina un producto del carrito
 
-    useEffect(() => {
-        // getShoppingApi es una funcion del componente de redux.
-        dispatch(getShoppingApi()).then(productos=> {
-            setProducts(productos.payload) // actualiza el estado de products con la respuesta a la api
-            setcontadorProductos(productos.payload.length)
-            if(!controller) { // controller evita que se duplique el monto total del carrito haciendo 2 peticiones api
-                controller = true
-                return montoTotal(productos.payload)
-            }
-        })
-    }, [])
+    let cantidadProduct = cantidad || 1,
+      precio = el.precio,
+      precioTotal = precioFinal,
+      id = el.id,
+      precioResultado = cantidadProduct * precio; // Multiplica por la cantidad de productos para generar un precio total
+    precioTotal -= precioResultado; // resta el precio total del proucto eliminado del precio total del carrito
 
-    const eliminarProducto = (el, cantidad) => { // Elimina un producto del carrito
+    let newData = products.filter((el) => {
+      // filtra el arr del carrito y lo devuelve sin el producto a eliminar
+      setcontadorProductos(contadorProductos - 1); // resta en 1 la cantidad de productos del carrito
+      return el.id !== id;
+    });
 
-        let cantidadProduct = cantidad || 1,
-            precio = el.precio,
-            precioTotal = precioFinal,
-            id = el.id,
-            precioResultado = cantidadProduct * precio // Multiplica por la cantidad de productos para generar un precio total
-        precioTotal -= precioResultado // resta el precio total del proucto eliminado del precio total del carrito
+    const si = arrClon.filter((el) => {
+      // elimina el elemento  del Arreglo Pricipal Clon "arrClon"
+      return el.disenio !== id;
+    });
 
-        let newData = products.filter((el) => { // filtra el arr del carrito y lo devuelve sin el producto a eliminar
-            setcontadorProductos(contadorProductos - 1) // resta en 1 la cantidad de productos del carrito
-            return el.id !== id
-        });
+    arrClon = si; // devuelve el dato filtrado y lo setea en arrClon
 
-        const si = arrClon.filter(el=>{ // elimina el elemento  del Arreglo Pricipal Clon "arrClon"
-            return el.disenio !== id
-        })
+    setprecioFinal(precioTotal); // Actualiza el precio total
+    setProducts(newData);
+  };
 
-        arrClon = si // devuelve el dato filtrado y lo setea en arrClon
+  const eliminarUnidad = (el) => {
+    // Elimina una unidad de un producto del carrito
+    let precioCarrito = precioFinal;
+    let precioProducto = (precioCarrito -= el.precio);
+    setprecioFinal(precioProducto); // devuelve el precioFinal con el valor de la unidad restada
+  };
 
-        setprecioFinal(precioTotal) // Actualiza el precio total
-        setProducts(newData)
-    }  
+  const añadirUnidad = (el) => {
+    // Agrega una unidad de un producto del carrito
+    let precioCarrito = precioFinal;
+    let precioProducto = (precioCarrito += el.precio);
+    setprecioFinal(precioProducto); // devuelve el precioFinal con el valor de la unidad añadida
+  };
 
-    const eliminarUnidad = (el) => { // Elimina una unidad de un producto del carrito
-        let precioCarrito = precioFinal
-        let precioProducto = precioCarrito -= el.precio
-        setprecioFinal(precioProducto) // devuelve el precioFinal con el valor de la unidad restada
-    }
+  const buyCart = () => {
+    const { email, id } = userLoged
+    axios(`api/sendMail/mailer/${id}`).then((result) => {
+      Swal.fire({
+        title: result.data,
+        imageUrl:
+          "https://icons.iconarchive.com/icons/google/noto-emoji-activities/256/52707-party-popper-icon.png",
+        imageWidth: 200,
+        imageHeight: 200,
+        imageAlt: "Party image",
+        width: 600,
+        showCloseButton: true,
+        showConfirmButton: false,
+        color: "black",
+        backdrop: ` rgba(0,0,123,0.4) `,
+        html: `You will be receiving a confirmation email whith order details in <a href="//${email}"><b>${email}"</b></a> `,
+      });
+    });
+  };
 
-    const añadirUnidad = (el) => { // Agrega una unidad de un producto del carrito
-        let precioCarrito = precioFinal
-        let precioProducto = precioCarrito += el.precio
-        setprecioFinal(precioProducto) // devuelve el precioFinal con el valor de la unidad añadida
-    }
-    
   return (
-    <div>
-        <div className="flex justify-between items-center px-12" style={{width:"90%", margin:"0 auto"}}>
+    <div className="flex justify-center items-center flex-col">
+      <div
+        className="flex justify-between items-center px-12"
+        style={{ width: "90%", margin: "0 auto" }}
+      >
         <h1>{`cantidad de productos: ${contadorProductos}`}</h1>
         <h1>{`Monto total: ${precioFinal}`}</h1>
-        </div>
-        <div className="Container-Grid-Body-F">{
-        products.length>0
-        ? products.map((el,index)=> <Card 
-            el={el}
-            key={index}
-            eliminarUnidad={eliminarUnidad} 
-            añadirUnidad={añadirUnidad} 
-            eliminarProducto={eliminarProducto}/>)
-        : <h1>Spinner</h1>
-        }
+      </div>
+      <div className="Container-Grid-Body-F">
+        {products.length > 0 ? (
+          products.map((el, index) => (
+            <Card
+              el={el}
+              key={index}
+              eliminarUnidad={eliminarUnidad}
+              añadirUnidad={añadirUnidad}
+              eliminarProducto={eliminarProducto}
+            />
+          ))
+        ) : (
+          <h1>Spinner</h1>
+        )}
+      </div>
+      <div className="flex justify-center align-center">
+        <button
+          href="#"
+          className="rounded-md bg-fuchsia-500 px-3.5 py-2.5 w-72 text-sm font-semibold text-white  shadow-sm hover:bg-fuchsia-700 "
+          onClick={buyCart}
+        >
+          BUY CART
+        </button>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Shopping
+export default Shopping;
